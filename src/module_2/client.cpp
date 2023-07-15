3ae79631c36e8422a1402538835a735ae40b6029
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2023-07-15 20:05:20 -0300

    a

--- src/module_2/client.cpp
+++ src/module_2/client.cpp
@@ -3,10 +3,12 @@
 #include "../module_1/socket.hpp"
 #include "./utils.hpp"
 
+#include <cstdio>
 #include <mutex>
 #include <thread>
 #include <condition_variable>
 
+std::mutex usingUserInput;
 std::mutex printMtx;
 std::mutex listenMtx;
 std::condition_variable cv;
@@ -15,68 +17,90 @@ int serverDescriptor = 0;
 bool listenning = false;
 bool listenningActive = false;
 
-void listenFunc() {
+std::string userInput = "";
+
+void listenFunc()
+{
 	// Monitor logic
-    std::unique_lock<std::mutex> lock(listenMtx);
+	std::unique_lock<std::mutex> lock(listenMtx);
 
-    while (!listenning) {
-        cv.wait(lock);
-    }
+	// std::cout << "listenning on!";
 
-	std::cout << "listenning on!";
+	while (listenningActive)
+	{
+		// std::cout << "listenning" << std::endl;
+		try
+		{
+			fd_set readSet;
+			FD_ZERO(&readSet);
+			FD_SET(serverDescriptor, &readSet);
 
-    while (listenningActive) {
-		std::cout << "listenning" << std::endl;
-        try {
-            fd_set readSet;
-            FD_ZERO(&readSet);
-            FD_SET(serverDescriptor, &readSet);
+			struct timeval timeout;
+			timeout.tv_sec = 1;	 // Set the timeout value in seconds
+			timeout.tv_usec = 0; // Set the timeout value in microseconds
 
-            struct timeval timeout;
-            timeout.tv_sec = 1; // Set the timeout value in seconds
-            timeout.tv_usec = 0; // Set the timeout value in microseconds
+			// Use select to wait for data or timeout
+			int selectResult = select(serverDescriptor + 1, &readSet, nullptr, nullptr, &timeout);
 
-            // Use select to wait for data or timeout
-            int selectResult = select(serverDescriptor + 1, &readSet, nullptr, nullptr, &timeout);
-
-            if (selectResult == -1) {
-                // Handle select error
-                // ...
+			if (selectResult == -1)
+			{
+				// Handle select error
+				// ...
 				; // pass
-            } else if (selectResult == 0) {
-                // Handle timeout, no data received within the specified timeout
-                // ...
+			}
+			else if (selectResult == 0)
+			{
+				// Handle timeout, no data received within the specified timeout
+				// ...
 				; // pass
-            } else {
-                // Data is available to read
-                if (FD_ISSET(serverDescriptor, &readSet)) {
+			}
+			else
+			{
+				// Data is available to read
+				if (FD_ISSET(serverDescriptor, &readSet))
+				{
+					std::string message = Socket::receive(serverDescriptor);
+					
 					std::unique_lock<std::mutex> lock(printMtx);
-                    std::string message = Socket::receive(serverDescriptor);
-                    std::cout << message;
-                }
-            }
-        } catch (const runtime_error &e) {
-			// TODO
-            break;
-        }
-    }
+					std::cout << "\r";
+					std::cout << "\033[K";
+					std::cout << "\r";
+
+					std::cout << message << std::endl;
+					std::cout << "> ";
+					std::cout.flush();
+					std::string userInputCopy;
+					{std::unique_lock<std::mutex> lock(usingUserInput);
+					userInputCopy = userInput;}
+					std::cout << userInputCopy;
+					std::cout.flush();
+				}
+			}
+		}
+		catch (const runtime_error &e)
+		{
+			std::unique_lock<std::mutex> lock(printMtx);
+			std::cerr << "Exception occurred: " << e.what() << std::endl;
+		}
+	}
 }
 
 // Function to put the thread to sleep
-void timeToGoDeaf () {
+void timeToGoDeaf()
+{
 	listenningActive = false;
-    std::unique_lock<std::mutex> lock(listenMtx);
+	std::unique_lock<std::mutex> lock(listenMtx);
 	listenning = false;
 }
 
 // Function to wake up the thread
-void timeToListen () {
+void timeToListen()
+{
 	listenningActive = true;
-    std::unique_lock<std::mutex> lock(listenMtx);
-    listenning = true;
-    cv.notify_one();
+	std::unique_lock<std::mutex> lock(listenMtx);
+	listenning = true;
+	cv.notify_one();
 }
-
 
 int main()
 {
@@ -90,67 +114,108 @@ int main()
 		bool connected = false;
 
 		listenning = false;
-		std::thread myListenner (listenFunc);
 
-		while (active) {
-			std::cout << "> ";
-			std::string message;
-			std::getline(std::cin, message);
-			//std::cout << "\n";
+		while (active)
+		{
+			{std::unique_lock<std::mutex> lock(printMtx);
+			std::cout << "> ";}
+
+			{
+				std::unique_lock<std::mutex> lock(usingUserInput);
+				userInput = "";
+			}
+			char incommingChar;
+
+			do
+			{
+				incommingChar = getchar();
+				if ('\n' != incommingChar) {
+					std::unique_lock<std::mutex> lock(usingUserInput);
+					userInput += incommingChar;
+				}
+			} while ('\n' != incommingChar);
+
+			// Clear the input buffer
+			// int c;
+			// do
+			// {
+			// 	c = getchar_unlocked();
+			// } while (c != EOF && c != '\n');
+
+			// std::cout << "\n";
 
 			// Meta commands
-			if (utils::hascmd(message, "/quit")) {
-				std::unique_lock<std::mutex> lock(printMtx);
-				std::cout << "- Quitting..." << std::endl;
+			std::string message;
+			{std::unique_lock<std::mutex> lock(usingUserInput); message = userInput;}
+			if (utils::hascmd(message, "/quit"))
+			{
+				{
+					std::unique_lock<std::mutex> lock(printMtx);
+					std::cout << "- Quitting..." << std::endl;
+				}
 				active = false;
 				clientSocket.send("/quit");
-			} else if (utils::hascmd(message, "/connect")) {
-				std::unique_lock<std::mutex> lock(printMtx);
+			}
+			else if (utils::hascmd(message, "/connect"))
+			{
 				connected = true;
-				std::cout << "- Conecting... " << std::endl;
+				{std::unique_lock<std::mutex> lock(printMtx);
+				std::cout << "- Conecting... " << std::endl;}
+
+				listenning = true;
+				listenningActive = true;
+				std::thread myListenner(listenFunc);
+				myListenner.detach();
 
 				// Connect to the server
 				clientSocket.connect();
 
 				serverDescriptor = clientSocket.getDescriptor();
-				timeToListen();
+				// timeToListen();
 
-				std::cout << "- Connection established... " << std::endl;
-			} 
+				{std::unique_lock<std::mutex> lock(printMtx);
+				std::cout << "- Connection established... " << std::endl;}
+			}
 
 			// check for connection
-			else if (!connected) {
+			else if (!connected)
+			{
 				std::unique_lock<std::mutex> lock(printMtx);
 				std::cout << "- [ERROR] You are not connected. " << std::endl;
 				// Dont execute the other if clouses if not connected
-			} 
-			
-			// other commands
-			else if (utils::hascmd(message, "/ping")) {
-				std::unique_lock<std::mutex> lock(printMtx);
-				clientSocket.send("/ping");
-				//std::string aaa = clientSocket.receive(clientSocket.getDescriptor());
-				//std::cout << aaa << std::endl;
-			} 
-			
-			// process regular message
-			else {
-				clientSocket.send(message);
-				std::unique_lock<std::mutex> lock(printMtx);
-				std::cout << "- Message sent to the server: '" 
-					<< message << "'" << std::endl ;
 			}
 
-			
-			std::cout << std::endl;
+			// other commands
+			else if (utils::hascmd(message, "/ping"))
+			{
+				//std::unique_lock<std::mutex> lock(printMtx);
+				clientSocket.send("/ping");
+				// std::string aaa = clientSocket.receive(clientSocket.getDescriptor());
+				// std::cout << aaa << std::endl;
+			}
+
+			// process regular message
+			else
+			{
+				clientSocket.send(message);
+				// std::unique_lock<std::mutex> lock(printMtx);
+				// std::cout << "- Message sent to the server: '"
+				// 		  << message << "'" << std::endl;
+			}
+
+			{std::unique_lock<std::mutex> lock(printMtx); std::cout << std::endl;}
 		}
 
-		timeToGoDeaf ();
-		myListenner.join();
-		std::cout << "- All done! Bye ;) " << std::endl;
+		// timeToGoDeaf ();
+		listenning = false;
+		listenningActive = false;
+
+		{std::unique_lock<std::mutex> lock(printMtx);
+		std::cout << "- All done! Bye ;) " << std::endl;}
 	}
 	catch (const std::exception &e)
 	{
+		//std::unique_lock<std::mutex> lock(printMtx);
 		std::cerr << "Exception occurred: " << e.what() << std::endl;
 	}
 
--- src/module_2/server.cpp
+++ src/module_2/server.cpp
@@ -4,8 +4,125 @@
 
 #include <mutex>
 #include <thread>
+#include <list>
+#include <condition_variable>
 
 
+std::mutex printMtx;
+
+std::list<int32_t> clients;
+std::list<std::thread> threads;
+
+std::mutex sendMsgMtx;
+std::condition_variable sendMsgCv;
+bool sending = false;
+
+std::mutex clientListMtx;
+std::condition_variable clientListCv;
+
+#define ALL (0)
+
+void removeClient(int32_t client) {
+	std::unique_lock<std::mutex> lock(clientListMtx);
+	clients.remove(client);
+}
+
+void addClient(int32_t client) {
+	std::unique_lock<std::mutex> lock(clientListMtx);
+	clients.push_back(client);
+}
+
+void sendMenssage(int32_t sender, std::string msg) {
+	std::unique_lock<std::mutex> lock(sendMsgMtx);
+
+	{std::unique_lock<std::mutex> lock(printMtx);
+	std::cerr << "- Sending msg: " << msg << std::endl;}
+
+	for (int32_t client : clients) {
+
+		if (client != sender) {
+			{std::unique_lock<std::mutex> lock(printMtx);
+				std::cerr << "- sending to client: " << client << std::endl;}
+			Socket::sendthis(client, msg);
+		}
+	}
+
+	{std::unique_lock<std::mutex> lock(printMtx);
+	std::cerr << "- Sent msg: " << msg << std::endl;}
+}
+
+void listenClientFunc (int32_t clientDescriptor, int index) {
+	// Receive and display messages from the client until the connection is closed or a timeout occurs
+	bool active = true;
+	while (active)
+	{
+		try
+		{
+			bool recieved = false;
+			std::string receivedMessage = "";
+			//std::string receivedMessage = Socket::receive(clientDescriptor);
+
+			fd_set readSet;
+            FD_ZERO(&readSet);
+            FD_SET(clientDescriptor, &readSet);
+
+            struct timeval timeout;
+            timeout.tv_sec = 1; // Set the timeout value in seconds
+            timeout.tv_usec = 0; // Set the timeout value in microseconds
+
+            // Use select to wait for data or timeout
+            int selectResult = select(clientDescriptor + 1, &readSet, nullptr, nullptr, &timeout);
+
+            if (selectResult == -1) {
+                // Handle select error
+                // ...
+				continue; // pass
+            } else if (selectResult == 0) {
+                // Handle timeout, no data received within the specified timeout
+                // ...
+				continue; // pass
+            } else {
+                // Data is available to read
+                if (FD_ISSET(clientDescriptor, &readSet)) {
+                    receivedMessage = Socket::receive(clientDescriptor);
+					recieved = true;
+                }
+            }
+
+			if (!recieved) continue;
+
+			if (utils::hascmd(receivedMessage, "/quit"))
+			{
+				active = false;
+			}
+			else if  (utils::hascmd(receivedMessage, "/connect")) {
+				; // pass
+			}
+			else if (utils::hascmd(receivedMessage, "/ping"))
+			{
+				// Send a "pong" response back to the client
+				sendMenssage(ALL, "pong");
+			} else {
+				sendMenssage(clientDescriptor, std::to_string(index) + ":" + receivedMessage);
+			}
+		}
+		catch (const std::runtime_error &e)
+		{
+			if (active) {
+				std::unique_lock<std::mutex> lock(printMtx);
+				std::cerr << "- [Error] " << e.what() << std::endl;
+			}
+			break; // Exit the inner loop if an error occurs during receiving
+		}
+	}
+
+	std::unique_lock<std::mutex> lock(printMtx);
+	std::cout << "- Client connection closed or timed out." << std::endl;
+
+	// Close the client socket
+	removeClient(clientDescriptor);
+	close(clientDescriptor);
+}
 
 int main()
 {
@@ -22,42 +139,21 @@ int main()
 
 		std::cout << "Server is listening for connections..." << std::endl;
 
-		for (;;)
+		
+		for (int index;; index++)
 		{
 			// Accept a client connection
 			int clientSocket = serverSocket.accept();
+			addClient(clientSocket);
 
-			// Receive and display messages from the client until the connection is closed or a timeout occurs
-			bool active = true;
-			while (active)
-			{
-				try
-				{
-					std::string receivedMessage = serverSocket.receive(clientSocket);
-					std::cout << "- Received message from the client: '" << receivedMessage << "'" << std::endl;
+			std::unique_lock<std::mutex> lock(printMtx);
+			std::cerr << "- Connected to " << clientSocket << std::endl;
+			
+			std::thread myThread([clientSocket, index](){
+				listenClientFunc(clientSocket, index);
+			});
 
-					if (utils::hascmd(receivedMessage, "/quit"))
-					{
-						active = false;
-					}
-					else if (utils::hascmd(receivedMessage, "/ping"))
-					{
-						// Send a "pong" response back to the client
-						Socket::sendthis(clientSocket, "pong");
-					}
-				}
-				catch (const std::runtime_error &e)
-				{
-					if (active)
-						std::cerr << "- [Error] " << e.what() << std::endl;
-					break; // Exit the inner loop if an error occurs during receiving
-				}
-			}
-
-			std::cout << "- Client connection closed or timed out." << std::endl;
-
-			// Close the client socket
-			close(clientSocket);
+			myThread.detach();
 		}
 	}
 	catch (const std::exception &e)
